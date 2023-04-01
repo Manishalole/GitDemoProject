9195f5a197a118db182ea05fd52746de2cd2f1ef
manisha <manu.lole.9@gmail.com>
2023-04-01 15:19:10 +0530

    commit files

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String handle = "yes";
+		if(handle == "yes")
+		{
+			System.out.println("i am hungry");
+			
+		}
+		else {
+			System.out.println("Not hungry");
+		}
+
+	}
+
+}
